91abc0d9e2af9d79a7115a6fa0bc557d0533c312
Ken M. Haggerty <eurekavalleyco@gmail.com>
2014-03-28 16:43:23 -0400

    Added LICENSE info to files.

--- UIViewController+Keyboard/UIViewController+Keyboard/AKSystemInfo.h
+++ UIViewController+Keyboard/UIViewController+Keyboard/AKSystemInfo.h
@@ -5,6 +5,9 @@
 //  Created by Ken M. Haggerty on 11/11/13.
 //  Copyright (c) 2013 Ken M. Haggerty All rights reserved.
 //
+//  For full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
+//
 
 #pragma mark - // NOTES (Public) //
 
